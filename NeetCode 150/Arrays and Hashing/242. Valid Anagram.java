45926ef4aa0ceef888741540d32a532a9cb664b6
SumitRodrigues <sumitrod11@gmail.com>
2024-11-14 18:34:05 -0800

    Using HashMap

--- NeetCode 150/Arrays and Hashing/242. Valid Anagram.java
+++ NeetCode 150/Arrays and Hashing/242. Valid Anagram.java
@@ -1,5 +1,6 @@
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 // Using Sorting
 public class Solution{
@@ -15,3 +16,37 @@ public class Solution{
         return Arrays.equals(sSort, tSort); 
     }
 }
+
+// Time & Space Complexity
+// Time complexity: 
+
+// O(nlogn)
+// Space complexity: 
+
+// O(1) or  O(n) depending on the sorting algorithm.
+
+// using hash table
+
+public class Solution2{
+    public boolean isAnagram(String s, String t){
+        if(s.length() == t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return sMap.equals(tMap);
+    }
+}
+
+// Time & Space Complexity
+// Time complexity: 
+
+// O(n)
+// Space complexity: 
+
+// O(1)
