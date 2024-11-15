7bbfe29a52d1a91a977ff83e8a3164be7499cab4
SumitRodrigues <sumitrod11@gmail.com>
2024-11-14 18:49:26 -0800

    49. Group Anagrams

---
+++ NeetCode 150/Arrays and Hashing/49. Group Anagrams.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+// Using sorting
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
+
+// Time & Space Complexity
+// Time complexity: 
+
+// O(m∗nlogn)
+// Space complexity: 
+
+// O(m∗n)
+
+// Using Hash Table
+
+public class Solution2 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
+
+// Time & Space Complexity
+// Time complexity: 
+
+// O(m∗n)
+// Space complexity: 
+
+// O(m)
