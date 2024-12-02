84d6fd23fc65e8f175d2df0fc69623f01fa816ee
SumitRodrigues <sumitrod11@gmail.com>
2024-12-02 06:16:35 -0800

    136 Using Brute Force

---
+++ NeetCode 150/Bit Manipulation/136. Single Number.java
@@ -0,0 +1,26 @@
+// Brute Force
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            boolean flag = true;
+            for(int j = 0; j < nums.length; j++){
+                if(i != j && nums[i] == nums[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+
+// Time & Space Complexity
+// Time complexity: 
+
+// O(n 2)
+// Space complexity: 
+
+// O(1)
