fb6dc805c2b0857047d5b512efeae4f6397d257c
SumitRodrigues <sumitrod11@gmail.com>
2025-02-16 08:02:59 -0800

    using iterative DFS

--- NeetCode 150/Trees/104. Maximum Depth of Binary Tree.java
+++ NeetCode 150/Trees/104. Maximum Depth of Binary Tree.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 import javax.swing.tree.TreeNode;
 
 /**
@@ -23,5 +25,27 @@ import javax.swing.tree.TreeNode;
             return 0;
         }
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
+ // Iterative DFS
+ class Solution2 {
+    public int maxDepth(TreeNode root) {
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(root, 1));
+        int res = 0;
+
+        while(!stack.isEmpty()){
+            Pair<TreeNode, Integer> current = stack.pop();
+            TreeNode node = current.getKey();
+            int depth = current.value();
+
+            while(root != null){
+                res = Math.max(res, depth);
+                stack.push(new Pair<>(root.left, depth + 1));
+                stack.push(new Pair<>(root.right, depth + 1));
+            }
+        }
+        return res;
     }
 }
