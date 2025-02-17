449ab3a2ba75b404e42124fdeceb060bd3118f29
SumitRodrigues <sumitrod11@gmail.com>
2025-02-16 17:30:26 -0800

    using dfs

--- NeetCode 150/Trees/543. Diameter of Binary Tree.java
+++ NeetCode 150/Trees/543. Diameter of Binary Tree.java
@@ -36,3 +36,24 @@ class Solution {
         return 1 + Math.max(maxHeight(root.left), maxHeight(root.right));
     }
 }
+
+// using DFS
+class Solution2 {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int[] res = new int[1];
+        dfs(root, res);
+        return res[0];
+    }
+    private int dfs(TreeNode root, int[] res){
+        if(root == null){
+            return 0;
+        }
+        int left = dfs(root.left, res);
+        int right = dfs(root.right, res);
+        res[0] = Math.max(res[0], left + right);
+        return 1 + Math.max(left, right);
+    }
+}
