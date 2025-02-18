e74fa0f965ab42fd832307a6d0cc06b43dc01d22
SumitRodrigues <sumitrod11@gmail.com>
2025-02-18 06:19:07 -0800

    using dfs

--- NeetCode 150/Trees/110. Balanced Binary Tree.java
+++ NeetCode 150/Trees/110. Balanced Binary Tree.java
@@ -34,3 +34,21 @@ class Solution {
         return 1 + Math.max(height(root.left), height(root.right));
     }
 }
+
+ // Using DFS
+ class Solution2 {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)[0] == 1;
+    }
+    private int[] dfs(TreeNode root){
+        if(root == null){
+            return new int[]{1, 0};
+
+        }
+        int[] left = dfs(root.left);
+        int[] right = dfs(root.right);
+        boolean balanced = (left[0] == 1 && right[0] == 1) && (Math.abs(left[1] - right[1]) < = 1);
+        int height = 1 + Math.max(left[1], right[1]);
+        return new int[]{balanced ? 1 : 0, height};
+    }
+}
